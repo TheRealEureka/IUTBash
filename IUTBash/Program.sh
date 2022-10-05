73eea164e966fdfcbc747d99d1575f5281cd0189
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-10-05 17:17:12 +0200

    welcum

---
+++ IUTBash/Program.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+num=$((1 + $RANDOM % 100))
+try=0
+
+
+echo "_________________________"
+echo "| Welcome to the game ! |"
+echo "|_______________________|"
+echo "  "
+echo "Guess a number between 1 and 100"
+echo "  "
+
+read guess
+
+
+
+
+while [ $guess -ne $num ]
+do
+    if [ $guess -lt $num ]
+    then
+        echo "Too low !"
+        try=$((try+1))
+    else
+        echo "Too high !"
+        try=$((try+1))
+    fi
+    read guess
+done
+try=$((try+1))
+echo "  "
+echo "  "
+echo "You guessed it !"
+echo "Your score is $try"
+echo "  "
+echo "You're weak, do better next time."
