c69976222713392239b2b6cf16f3f5bcd22764ae
lucasfarroni <lucas.farroni@thedots.lu>
2022-10-19 20:12:46 +0200

    saveScore

--- ExosJs/script.js
+++ ExosJs/script.js
@@ -8,6 +8,10 @@
 
 let name = "";
 let numRandom = Math.floor(Math.random() * 100) + 1;
+let Bestscore = { name : name ,
+    score : 0
+}
+let tab = [];
 
 //document.getElementById('btn').addEventListener('click',function (){
 //let message = prompt("What's your name human?")
@@ -16,6 +20,14 @@ let numRandom = Math.floor(Math.random() * 100) + 1;
 //    }
 //    console.log(message)
 //});
+
+function save(obj) {
+    tab.push(obj);
+    tab.sort((a ,b) => {
+        return a.score - b.score;
+    });
+    localStorage.setItem("scoreFile", JSON.stringify(tab));
+}
 
 
 
@@ -28,18 +40,30 @@ document.getElementById('start').addEventListener('click',function (){
     document.getElementById('submit').addEventListener('click',function (){
         console.log('submit');
         let number = document.getElementById('numberInput').value;
+        Bestscore.name=name;
+        Bestscore.score+=1;
         console.log(numRandom);
         if (number > numRandom){
             document.getElementById('result').innerHTML = "Le nombre est plus petit, Choisi un nombre entre 1 et 99";
             console.log("Le nombre est plus petit, Choisi un nombre entre 1 et 99");
+            Bestscore.score+=1;
         } else if (number < numRandom){
             console.log("Le nombre est plus grand, Choisi un nombre entre 1 et 99");
             document.getElementById('result').innerHTML = "Le nombre est plus grand, Choisi un nombre entre 1 et 99";
+            Bestscore.score+=1;
         } else {
             console.log("Bravo tu as trouvé le nombre mystère");
             document.getElementById('result').innerHTML = '<p>Bravo tu as trouvé le nombre mystère</p>';
             document.getElementById('submit').style.display = 'none';
             document.getElementById('numberInput').style.display = 'none';
+
+            save(Bestscore);
+            let data = localStorage.getItem("scoreFile");
+            let TabScore = JSON.parse(data);
+            document.getElementById("PlayerName").innerHTML = TabScore.forEach((e) => {
+                console.log(`${e.score} ${e.name}`)
+            });
+
         }
     });
 });
